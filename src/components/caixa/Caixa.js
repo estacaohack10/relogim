d85a08af4f59f7fc40d54e30ae9ead57cc52b3f8
Bruno Pimentel <brunoapimentel@gmail.com>
2018-11-28 21:28:44 -0200

    Implementa contador

--- src/components/caixa/Caixa.js
+++ src/components/caixa/Caixa.js
@@ -4,10 +4,43 @@ import Timer from '../timer/Timer';
 import Botao from '../botao/Botao';
 
 class Caixa extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            minutos: 0,
+            segundos: 10
+        }
+
+        this.decrementar();
+    }
+
+    decrementar(){
+        let intervalo = setInterval(() => {
+            let meusSegundos = this.state.segundos;
+            let meusMinutos = this.state.minutos;
+
+            if(meusMinutos === 0 && meusSegundos === 0){
+                clearInterval(intervalo);
+            }else if(meusSegundos === 0){
+                meusSegundos = 59;
+                meusMinutos--;
+            }else{
+                meusSegundos--;
+            }
+
+            this.setState({
+                minutos: meusMinutos,
+                segundos: meusSegundos
+            });
+        }, 1000);
+    }
+
     render(){
         return <section className="Caixa">
             <div className="main">
-                <Timer minutos="10" segundos="23"/>
+                <Timer minutos={this.state.minutos} segundos={this.state.segundos}/>
 
                 <div className="buttons">
                     <Botao conteudo="+"/>
